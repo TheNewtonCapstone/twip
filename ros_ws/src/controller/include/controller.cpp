e242894a4d868257ddb8c42e547beb3811fde400
ivan <shamiivan@gmail.com>
2024-07-08 12:37:59 -0400

    wip : Modify onnx configurations

--- ros_ws/src/controller/include/controller.cpp
+++ ros_ws/src/controller/include/controller.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cmath>
 #include <cstdint>
 #include <onnxruntime_c_api.h>
@@ -6,38 +7,51 @@
 class OnnxController {
 
 public:
-  OnnxController() : observations_(), actions_(), model_path(path) {
+  OnnxController(const std::string path, const int observations, const int actions) 
+    : observations_(observations), actions_(actions), model_path_(path) {
+    
+
     input_buffer_ = new float[observations_]();
     output_buffer_ = new float[actions_]();
 
     // initialize the onnx runtime sessions
     Ort::SessionOptions options;
-    Ort::CUDAProvideOptions cuda_options;
+    OrtCUDAProviderOptions cuda_options;
 
     cuda_options.device_id = 0;
-    cuda_options.cudmm_con_algo_search = EXHAUSTIVE;
+    cuda_options.cudnn_conv_algo_search = EXHAUSTIVE;
     cuda_options.gpu_mem_limit = SIZE_MAX;
     cuda_options.arena_extend_strategy = 0;
     cuda_options.do_copy_in_default_stream = 0;
-    cuda_options.has_user_compute_stream = nullptr;
+    cuda_options.has_user_compute_stream = 0;
     cuda_options.user_compute_stream = nullptr;
     cuda_options.default_memory_arena_cfg = nullptr;
     options.AppendExecutionProvider_CUDA(cuda_options);
 
     // create session
-    session_ = Ort::Session{env_, model_path_.c_str(), options}
+    session_ = Ort::Session{env_, model_path_.c_str(), options};
 
     input_shape_ = {observations_};
     output_shape_ = {actions};
 
-    auto memory_info =
-        Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
-    input_tensor = Ort::Value::CreateTensor<float>(
-        memory_info, input_buffer_, observations_, input_shape_.data(),
-        input_shape_.size());
+    auto memory_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
+    std::cout << "Memory information : \n"  << memory_info << std::endl;
+
+    input_tensor_ = Ort::Value::CreateTensor<float>(
+      memory_info, input_buffer_, observations_, input_shape_.data(),
+      input_shape_.size());
+    
     output_tensor_ = Ort::Value::CreateTensor<float>(
-        memory_info, output_buffer_, actions_, output_shape_.data(),
-        output_shape_.size());
+      memory_info, output_buffer_, actions_, output_shape_.data(),
+      output_shape_.size());
+    
+
+
+    std::cout << "Input and output buffers ahve been initiliazed!\n"  
+              << memory_info << std::endl
+              << input_tensor_ << std::endl
+              << output_tensor_ << std::endl;
+
   }
   ~OnnxController() {
     delete[] input_buffer_;
@@ -69,13 +83,25 @@ public:
   };
 
   // get pointers to the input and output of the buffers
-  std::pair<float *float *> get_buffers() {
-    std::pair<float *, float *>(input_buffer_, output_buffer_);
+  std::pair<float* , float *> get_buffers() {
+  std::pair<float *, float *>(input_buffer_, output_buffer_);
   }
 
-  int control_step(float dt) { return 0; }
+  int control_step(float dt) { 
+    const char *input_names[] = {"observations"};
+    const char *output_names[] = {"actions"};
+    
+   int num_input_tensors = 1;
+   int num_output_tensors = 1;
+    session_.Run(opt_, 
+                input_names, &input_tensor_, num_input_tensors, 
+                output_names, &output_tensor_, num_output_tensors);
+
+    return 0; 
+    }
 
 private:
+  std::string model_path;
   int observations_;
   int actions_;
 
@@ -83,18 +109,19 @@ private:
   float *output_buffer_ = nullptr;
 
   // vectors to store pre and post transform rulesl
-  std::vectod<TransformRule<float, float>> pre_tranforms_;
-  std::vector < TransformRule<float, float> post_transforms_;
+  std::vector<TransformRule<float, float>> pre_tranforms_;
+  std::vector < TransformRule<float, float>> post_transforms_;
 
   // onnx
   std::string model_path_;
 
-  Ort::env env_;
+  Ort::Env env_;
   Ort::Session session_{nullptr};
   Ort::RunOptions opt_{nullptr};
 
   // input and output
-  Ort::value input_tensor_{nullptr} Ort::array<int64_t, 1> input_shape_;
+  Ort::Value input_tensor_{nullptr};
+  std::array<int64_t, 1> input_shape_;
   Ort::Value output_tensor_{nullptr};
   std::array<int64_t, 1> output_shape_;
 };
--- ros_ws/src/controller/include/memory_manager.hpp
+++ ros_ws/src/controller/include/memory_manager.hpp
@@ -1,7 +1,7 @@
 /*
  * Description:
- *  Load and config memory 
- *  Create and control shared memory buffers 
+ *  Load and config memory
+ *  Create and control shared memory buffers
  *  mem
  **/
 #pragma once
@@ -22,131 +22,147 @@
 #include <experimental/optional>
 #include <controller.cpp>
 
-
-#define display_err_exit(msg)        \
-    do                      \
-    {                       \
-            perror(msg);        \
-            exit(EXIT_FAILURE); \
+#define display_err_exit(msg) \
+    do                        \
+    {                         \
+        perror(msg);          \
+        exit(EXIT_FAILURE);   \
     } while (0)
 
-struct MemoryConfig {
-    MemoryConfig(const std::string &config_path){
+struct MemoryConfig
+{
+    MemoryConfig(const std::string &config_path)
+    {
         YAML::Node config = YAML::LoadFile(config_path);
 
         auto buffers = config["buffers"];
-        for(YAML::const_iterator it=buffers.begin(); it!= buffers.end(); ++it){
+        for (YAML::const_iterator it = buffers.begin(); it != buffers.end(); ++it)
+        {
             buffer_config.emplace(it->first.as<std::string>(), it->second.as<int>());
         }
         auto controllers_ = config["controllers"];
-        for(YAML::const_iterator it=buffers.begin(); it!= buffers.end(); ++it){
+        for (YAML::const_iterator it = buffers.begin(); it != buffers.end(); ++it)
+        {
             controller_config.emplace(it->first.as<std::string>(), it->second);
         }
     }
-    std::map<std::string,int> buffer_config;
+    std::map<std::string, int> buffer_config;
     std::map<std::string, YAML::Node> controller_config;
 };
 
+class MemoryManager
+{
+public:
+    MemoryManager(const std::string &config_path) : cfg_(config_path)
+    {
 
-class MemoryManager{
-    public:
-        MemoryManager(const std::string &config_path) : cfg_(config_path){
+        if (mlockall(MCL_FUTURE) == -1)
+            display_err_exit("Failed to lock memeory pages with mlockall");
 
-            if(mlockall(MCL_FUTURE) == -1)
-                display_err_exit("Failed to lock memeory pages with mlockall");
+        // create and map shared memory segments
+        for (std::map<std::string, int>::iterator it = cfg_.buffer_config.begin();
+             it != cfg_.buffer_config.end(); ++it)
+        {
+            const char *name = it->first.c_str();
+            const int SIZE = it->second;
 
+            int shm_file_desc;
+            void *ptr;
 
-            //create and map shared memory segments 
-            for(std::map<std::string, int>::iterator it = cfg_.buffer_config.begin();
-                    it != cfg_.buffer_config.end(); ++it){
-                const char *name = it->first.c_str();
-                const int SIZE = it->second;
+            // create object
+            shm_file_desc = shm_open(name, O_CREAT | O_RDWR, S_IRWXU | S_IRWXO);
+            if (shm_file_desc == -1)
+                display_err_exit(("Failed to create shared memory object: " + std::string(name)).c_str());
 
-                int shm_file_desc;
-                void *ptr;
+            if (ftruncate(shm_file_desc, SIZE) == -1)
+                display_err_exit(("Failed to set size of the memory object: " + std::string(name)).c_str());
 
-                 // create object 
-                 shm_file_desc= shm_open(name, O_CREAT | O_RDWR, S_IRWXU | S_IRWXO);
-                 if(shm_file_desc == -1) 
-                    display_err_exit(("Failed to create shared memory object: " + std::string(name)).c_str());
+            // map the share memeory objectto the process's address space
+            ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_file_desc, 0);
+            if (ptr == MAP_FAILED)
+                display_err_exit(("Failed to map shared memory file desciptor" + std::string(name)).c_str());
 
-                if(ftruncate(shm_file_desc, SIZE) == -1)
-                    display_err_exit(("Failed to set size of the memory object: " + std::string(name)).c_str());
+            if (close(shm_file_desc) == -1)
+                display_err_exit(("Failed ot close shared memory file descriptor" + std::string(name)).c_str());
 
-                // map the share memeory objectto the process's address space 
-                ptr = nmap(0, SIZE, PROT_WRITE, MAP_SHARED,shm_file_desc, 0);
-                if(ptr == MAP_FAILED)
-                    display_err_exit(("Failed to map shared memory file desciptor" + std::string(name)).c_str());
+            for (std::map<std::string, YAML::Node>::iterator it = cfg_.controller_config.begin(); it != cfg_.controller_config.end(); ++it)
+            {
 
-                if(close(shm_file_desc) == -1)
-                    display_err_exit(("Failed ot close shared memory file descriptor" + std::string(name)).c_str());
+                if (it->second["type"].as<std::string>() == "onnx")
+                {
+                    auto &dict = it->second;
+                    int observations = dict["observations"].as<int>();
+                    int actions = dict["actions"].as<int>();
+                    std::string model_path = dict["model_path"].as<std::string>();
 
-                for(std::map<std::string, YAML::Node>::iterator it = cfg_.controller_config.begin(); it != cfg_.controller_config.end(); ++it){
+                    std::shared_ptr<OnnxController> onnx_controller = std::make_shared<OnnxController>(observations, actions, model_path);
 
-                    if(it->second["type"].as<std::string>() == "onnx"){
-                        auto &dict = it->second;
-                        int observations  = dict["observations"].as<int>();
-                        int actions = dict["actions"].as<int>();
-                        std::string model_path =dict["model_path"].as<std::string>();
-
-                        std::shared_ptr<OnnxController> onnx_controller =std::maked_shared<OnnxController>(observations, actions, model_path);
-
-                        controllers_.emplace(it->first, onnx_controller);
-                    }
+                    controllers_.emplace(it->first, onnx_controller);
                 }
-
+            }
         }
+    }
 
-        ~MemoryManager(){
-            //unlink shared memory segments 
-            for(const auto& buffer : cfg_.buffer_config){
-                const char* segment_name = buffer.first.c_str();
-                if(shm_unlink(segment_name) == -1)
+        ~MemoryManager()
+        {
+            // unlink shared memory segments
+            for (const auto &buffer : cfg_.buffer_config)
+            {
+                const char *segment_name = buffer.first.c_str();
+                if (shm_unlink(segment_name) == -1)
                     display_err_exit(("Failed to unlink shared memory" + std::string(segment_name)).c_str());
             }
 
-            // 
-            if(munlockall() == -1)
+            //
+            if (munlockall() == -1)
                 display_err_exit("Failed to unlock memeory pages");
-        } 
-
-            std::experimental::optional<std::shared_ptr<OnnxController> getController(const std::string &name){
-                auto it = controllers_.find(name);
-                //check if controller not found, return an nulloptional(does not contain any values)
-                if(it ==controllers.end()){
-                    return std::experimental::nullopt
-                }
-                return it->second;
-            }
-
-
-            /*
-            * Create a buffer for read or write operation 
-            * rw=0 for read
-            * rw=1 for write
-            */
-           template <class T>
-           int create_buffer(const char *name, const int &rw, T *&ptr){
-            size_t buffer_size = cfg_.buffer_config.at(name);
-            int shared_memory_fd = shm_open(name, O_RDWR, S_IRWXU, S_IRWXO);
-            if(shared_memory_fd == -1)
-                display_err_exit(("Failed to open shared memory object: " + std::string(name)).c_str());
-           }
-
-           //prot for protection
-           int protection_level = (rw == 0) ? PROT_READ : PROT_WRITE;
-           void *mapped_memory = nmap(0, buffer_size, protection_level, MAP_SHARED, shared_memory_fd,0);
-
-
-           
-
-
-
-
-
-           return 0; 
         }
-        private:
-            MemoryConfig cfg_;
-            std::map<std::string, std:;shared_ptr<OnnxController>>controllers_;
+
+        std::experimental::optional < std::shared_ptr<OnnxController>> getController(const std::string &name)
+        {
+            auto it = controllers_.find(name);
+            // check if controller not found, return an nulloptional(does not contain any values)
+            if (it == controllers.end())
+            {
+                return std::experimental::nullopt
+            };
+            return it->second;
+        }
+
+        /*
+         * Create a buffer for read or write operation
+         * rw=0 for read
+         * rw=1 for write
+         */
+        template <class T>
+        int create_buffer(const char *name, const int &rw, T *&ptr)
+        {
+            size_t buffer_size = cfg_.buffer_config.at(name);
+            int shm_fd = shm_open(name, O_RDWR, S_IRWXU, S_IRWXO); 
+            if (shm_fd == -1)
+                display_err_exit(("Failed to open shared memory object: " + std::string(name)).c_str());
+
+            // implement demand paging
+            int protection_level = (rw == 0) ? PROT_READ : PROT_WRITE;
+            void *mapped_memory = mmap(0, buffer_size, protection_level, MAP_SHARED, shm_fd, 0);
+
+            if (mapped_memory == MAP_FAILED)
+                display_err_exit(("Failed to map the shared memory buffer" + std::string(name)).c_str());
+            
+
+            if(close(shm_fd) == -1)
+                display_err_exit(("Failed to close the shared memory file descriptor" + std::string(name)).c_str());
+
+
+            size_t n_elements = buffer_size / sizeof(T);
+            ptr = static_cast<T*>(mapped_memory);
+
+            return n_elements;
+        };
+
+
+    private:
+        MemoryConfig cfg_;
+        std::map < std::string, std::shared_ptr < OnnxController >> controllers_;
+
 };
