80f8566824b104dcbc6e7bcfefc503ede82fd855
ivan <shamiivan@gmail.com>
2024-08-29 01:12:26 -0400

    style : refactored th efile to be more clean

--- ros_ws/src/controller/include/onnx_handler.hpp
+++ ros_ws/src/controller/include/onnx_handler.hpp
@@ -7,37 +7,37 @@
 #include <onnxruntime_c_api.h>
 #include <onnxruntime_cxx_api.h>
 
-class OnnxHandler{
-    public:
-        OnnxHandler(const std::string _path, const int _num_inputs, const int _num_outputs);
-        void run(); 
-        void init_onnx_session();
-        std::vector<float>& get_input_buffer();
-        std::vector<float>& get_output_buffer();      
+class OnnxHandler {
+public:
+    OnnxHandler(const std::string _path, const int _num_inputs, const int _num_outputs);
+    void run();
+    void init_onnx_session();
+    std::vector<float>& get_input_buffer();
+    std::vector<float>& get_output_buffer();
 
-    private:
+private:
 
-        static constexpr const char* INPUT_NAME = "observations";
-        static constexpr const char* OUTPUT_NAME = "actions";
+    static constexpr const char* INPUT_NAME = "observations";
+    static constexpr const char* OUTPUT_NAME = "actions";
 
-        std::string path;
-        int num_inputs;
-        int num_outputs;
+    std::string path;
+    int num_inputs;
+    int num_outputs;
 
-        Ort::Env env;
-        Ort::Session session{nullptr};
-        Ort::RunOptions opt{nullptr};
+    Ort::Env env;
+    Ort::Session session{ nullptr };
+    Ort::RunOptions opt{ nullptr };
 
-    
-        Ort::Value input_tensor{nullptr};
-        // shape specifies the dimensions of the input/output sensor
-        std::array<int64_t, 1> input_shape;
-        std::vector<float> input_buffer;
-    
 
-        Ort::Value output_tensor{nullptr};
-        std::array<int64_t, 1> output_shape;
-        std::vector<float> output_buffer;
+    Ort::Value input_tensor{ nullptr };
+    // shape specifies the dimensions of the input/output sensor
+    std::array<int64_t, 1> input_shape;
+    std::vector<float> input_buffer;
+
+
+    Ort::Value output_tensor{ nullptr };
+    std::array<int64_t, 1> output_shape;
+    std::vector<float> output_buffer;
 
 
 };
