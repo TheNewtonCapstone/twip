9f7b210f94067e3fb04ef934f2b00d494a5cf5af
ivan <shamiivan@gmail.com>
2024-10-10 22:33:50 -0400

    update setup

--- setup.bash
+++ setup.bash
@@ -1,18 +1,18 @@
 #! /bin/bash
 
-
+work_dir=$(pwd)
+echo "working dir is ${work_dir}"
 function run_mr_agent(){
     docker run -it --rm --net=host -v /dev/shm/:/dev/shm  --privileged -v /dev:/dev microros/micro-ros-agent:humble serial --dev /dev/ttyUSB0 -v6
-
 }
 
 function run_onnx(){
-docker run -it --net=host \
-    -e DISPLAY=$DISPLAY \
-    -v /dev:/dev \
-    --device-cgroup-rule='c *:* rmw' \
-    --device=/dev \
-    -v /home/nyquist/workspace/twip:/~ \
-    twip:l4t-onnx-ros-r36.3.0 \
+    docker run -it --net=host \
+        -e DISPLAY=$DISPLAY \
+        -v /dev:/dev \
+        --device-cgroup-rule='c *:* rmw' \
+        --device=/dev \
+        -v /${work_dir}:/${work_dir}\
+        twip:l4t-onnx-ros-r36.3.0 \
     bin/bash
 }
